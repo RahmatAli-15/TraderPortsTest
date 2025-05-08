958a95210d5dd716e98aa15f0a8652922f23d368
Rahmat Ali <rahmatali20042002@gmail.com>
2025-05-08 10:14:11 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,13 @@ const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
 const ExpenseRouter = require('./Routes/ExpenseRouter');
 const ensureAuthenticated = require('./Middlewares/Auth');
+const path = require("path");
 
 require('dotenv').config();
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
+
+const _dirname = path.resolve();
 
 app.get('/ping', (req, res) => {
     res.send('PONG');
@@ -20,6 +23,11 @@ app.use(cors());
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 app.use('/expenses', ensureAuthenticated, ExpenseRouter)
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+})
 
 
 app.listen(PORT, () => {
